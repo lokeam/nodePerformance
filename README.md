b770c9706f77c9b6b77b3406609780aae0da833d
Ahnming Loke <lokeahnming@gmail.com>
2019-12-08 11:21:46 -0500

    test: PM2 install + cheatsheet documentation

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-Exploring node clustering, worker threads, benchmarking server performance and other run topics.
+## Exploring node clustering, worker threads, benchmarking server performance and other run topics.
 
 Not using nodemon since it doesn't work very nicely with clustering.
 
 Benchmarking conducting with ab - [Apache HTTP server benchmarking tool](https://httpd.apache.org/docs/2.4/programs/ab.html)
+
+Process managing with [PM2](https://pm2.keymetrics.io/) daemon process manager.
+
+### PM2 Cheatsheet:
+
+`pm2 list` - summary of instances and related cluster health
+`pm2 show` [*name of instance*] - deep dive information for specific server instance
+`pm2 monit` - creates dashboard of processes
+`pm2 delete` [*name of instance*] - stops a specific instance
--- index.js
+++ index.js
@@ -1,37 +1,24 @@
-/* Restrict available threads for every forked child to 1 */
-process.env.UV_THREADPOOL_SIZE = 1;
-
-const cluster = require('cluster');
 const express = require('express');
 const crypto = require('crypto');
 const app = express();
 
 /* Testing cluster Manager, will always set to true. Once cluster.fork() is called, will return false */
 
-/* Is file being executed in master mode? */
-if (cluster.isMaster) {
-  /* Cause index.js to be executed again in child mode. Each child will have separate thread pool */
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
-} else {
-  /* Child mode use case, will act like a server */
+/* Child mode use case, will act like a server */
 
-  app.get('/', (request, response) => {
-    /* Handled inside event loop, we're stuck here doing the things until this fn completes */
+app.get('/', (request, response) => {
+  /* Handled inside event loop, we're stuck here doing the things until this fn completes */
 
-    /* Test work function. Calculate hash then send response back */
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-      console.log('Finished doing work');
-    });
+  /* Test work function. Calculate hash then send response back */
+  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('Finished doing work');
   });
+});
 
-  app.get('/fast', (request, response) => {
-    response.send('This loaded *fast*');
-  });
+app.get('/fast', (request, response) => {
+  response.send('This loaded *fast*');
+});
 
-  app.listen(3000);
-}
+app.listen(3000);
 
 
